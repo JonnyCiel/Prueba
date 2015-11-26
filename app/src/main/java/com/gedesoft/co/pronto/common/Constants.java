16e8223d2f64771e03d87c828a48493bda31e685
Jonny <jonny.lozano@hotmail.com>
2015-11-25 20:07:42 -0500

    first commit

---
+++ app/src/main/java/com/gedesoft/co/pronto/common/Constants.java
@@ -0,0 +1,14 @@
+package com.gedesoft.co.pronto.common;
+
+/**
+ * Created by Jonny on 24/11/2015.
+ */
+public class Constants {
+
+    public final static int NOTEPAD = 1;
+    public final static int TODO = 2;
+    public final static int DRAWING = 3;
+    public final static int REMINDER = 4;
+    public final static int MOVIE = 5;
+    public final static int SETTINGS = 6;
+}
